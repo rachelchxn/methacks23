a2fe6696d34dca174fe0c7e94c9b0ca2f521f70a
Kelly Qu <116982102+kellyquu@users.noreply.github.com>
2023-05-06 12:58:39 -0400

    materalUI, popup.js

--- methacks23/src/App.js
+++ methacks23/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect } from 'react';
+import { Popup } from './Popup';
 
 // THIS IS JUST DATA FOR THE COHERE API
 const options = {
@@ -59,20 +60,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Popup/>
     </div>
   );
 }
--- methacks23/src/Popup.js
+++ methacks23/src/Popup.js
@@ -1,13 +1,28 @@
-import React from 'react'
-import Logo from './logo'
+import React, { useState } from 'react';
+import Toggle from 'react-toggle';
+import 'react-toggle/style.css';
+import './custom-toggle.css';
+
+import Logo from './logo.svg';
 
 export const Popup = () => {
+  const [isToggled, setIsToggled] = useState(false);
+
+  const handleToggle = () => {
+    setIsToggled(!isToggled);
+  }
+
   return (
     <div>
-        <div className='header'>
-            <img src={Logo} alt="Logo"/>
-            <h3>ZenSphere</h3>
-        </div>
+      <div className='header'>
+        <img src={Logo} alt="Logo"/>
+        <h3>ZenSphere</h3>
+        <Toggle
+          checked={isToggled}
+          onChange={handleToggle}
+          icons={false}
+        />
+      </div>
     </div>
   )
 }
