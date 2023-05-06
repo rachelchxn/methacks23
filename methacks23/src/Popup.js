9fec391dd6472477373763966f394dc97503f9a1
rachelchxn <85042944+rachelchxn@users.noreply.github.com>
2023-05-06 03:03:56 -0400

    lol little front end

---
+++ methacks23/src/Popup.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Logo from './logo'
+
+export const Popup = () => {
+  return (
+    <div>
+        <div className='header'>
+            <img src={Logo} alt="Logo"/>
+            <h3>ZenSphere</h3>
+        </div>
+    </div>
+  )
+}
