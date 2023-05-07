79050a489f2996a0ac27d437e4eeeab268b79c48
Kelly Qu <116982102+kellyquu@users.noreply.github.com>
2023-05-07 04:23:47 -0400

    Update App.js

--- methacks23/src/App.js
+++ methacks23/src/App.js
@@ -2,12 +2,14 @@ import Logo from './logo.svg';
 import {Popup} from './Popup.js';
 import './App.css'
 
-
 function App() {
-
   return (
     <div className="App">
-      <Popup />
+      <header>
+        <img src={Logo} alt="Logo"/>
+        <h3>ZenSphere</h3>
+      </header>
+      <Popup/>
       <footer>
         <button className="submit"> Submit </button>
       </footer>
