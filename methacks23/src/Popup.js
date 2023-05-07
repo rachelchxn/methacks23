4b5000f1d8144b205224b9f2bc1510d7b193fa3e
Kelly Qu <116982102+kellyquu@users.noreply.github.com>
2023-05-06 21:29:17 -0400

    TAGS

--- methacks23/src/Popup.js
+++ methacks23/src/Popup.js
@@ -4,19 +4,31 @@ import 'react-toggle/style.css';
 import Logo from './logo.svg';
 import './Popup.css';
 
-function Popup() {
+export const Popup = () => {
   const [isToggled, setIsToggled] = useState(false);
+  const [tags, setTags] = useState([]);
+  const [inputValue, setInputValue] = useState('');
 
   const handleToggle = () => {
     setIsToggled(!isToggled);
   }
 
   const handleSubmit = () => {
-    // Handle form submission logic here
+    // Add the input value to the tags array
+    setTags([...tags, inputValue]);
+
+    // Clear the input value
+    setInputValue('');
   }
 
   const handleReset = () => {
-    // Handle form reset logic here
+    // Clear the tags array and input value
+    setTags([]);
+    setInputValue('');
+  }
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
   }
 
   return (
@@ -24,12 +36,36 @@ function Popup() {
       <div className='header'>
         <img src={Logo} alt="Logo"/>
         <h3>ZenSphere</h3>
-        <Toggle
-          checked={isToggled}
-          onChange={handleToggle}
-          icons={false}
-        />
       </div>
+      
+      <p1>Input keywords of the type of content you wish to filter.</p1>
+
+      <h5>Keywords</h5>
+
+      <div className='form'>
+        {/* Text box */}
+        
+        <div className='input-container'>
+          <input
+            type='text'
+            placeholder='Type here...'
+            value={inputValue}
+            onChange={handleInputChange}
+          />
+          <button className='submit-button' onClick={handleSubmit}>Add tag</button>
+        </div>
+
+        {/* Tag list */}
+        <div className='tag-list'>
+          {tags.map(tag => (
+            <div key={tag} className='tag'>{tag}</div>
+          ))}
+        </div>
+
+        {/* Reset button */}
+        <button className='reset-button' onClick={handleReset}>Clear Tags</button>
+      </div>
+      
       <h4>Image Filter</h4>
         <Toggle
           checked={isToggled}
@@ -42,16 +78,7 @@ function Popup() {
           onChange={handleToggle}
           icons={false}
         />
-      <div className='form'>
-        {/* Submit button */}
-        <button className='submit-button' onClick={handleSubmit}>Submit</button>
-        
-        {/* Reset button */}
-        <button className='reset-button' onClick={handleReset}>Reset</button>
-      </div>
+
     </div>
   )
-  
 }
-
-export {Popup};
